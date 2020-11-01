72f917acaeea075861729b5b28d969c84344eeb1
Sean Owen <sean_owen@live.co.uk>
2020-11-01 00:56:51 +0000

    - completed CamelCaseMethod kata

---
+++ CodeWars/ManualTesting/CamelCaseMethod/CamelCaseMethodKata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KataSolutions.CamelCaseMethod
+{
+    public static class CamelCaseMethodKata
+    {
+        public static string CamelCase(this string str)
+        {
+            string camelCased = string.Empty;
+            string[] wordsArray = str.Split(' ');
+
+            foreach(string word in wordsArray)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if(i == 0)
+                    {
+                        camelCased += word.ToUpper()[i];
+                    }
+                    else
+                    {
+                        camelCased += word.ToLower()[i];
+                    }
+                }
+            }
+            return camelCased;
+        }
+    }
+}
---
+++ CodeWars/UnitTests/CamelCaseMethodTests.cs
@@ -0,0 +1,20 @@
+﻿    using NUnit.Framework;
+    using System;
+    using KataSolutions.CamelCaseMethod;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class CamelCaseMethodTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("TestCase", "test case".CamelCase());
+            Assert.AreEqual("CamelCaseMethod", "camel case method".CamelCase());
+            Assert.AreEqual("SayHello", "say hello".CamelCase());
+            Assert.AreEqual("CamelCaseWord", " camel case word".CamelCase());
+            Assert.AreEqual("", "".CamelCase());
+        }
+    }
+}
