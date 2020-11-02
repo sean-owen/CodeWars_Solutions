b62ca63c944468c7c5ce1ee6f811afc7a387e02f
Sean Owen <sean_owen@live.co.uk>
2020-11-02 14:17:58 +0000

    - solve mexican wave kata

---
+++ CodeWars/ManualTesting/MexicanWave/MexicanWaveKata.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KataSolutions.MexicanWave
+{
+    public class MexicanWaveKata
+    {
+        public List<string> Wave(string str)
+        {
+            str = str.ToLower();            
+            List<string> strWaveList = new List<string>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != ' ')
+                {
+                    char[] strArray = str.ToCharArray();
+                    strArray[i] = Char.ToUpper(strArray[i]);
+                    strWaveList.Add(new string(strArray));
+                }
+            }
+
+            return strWaveList;
+        }
+    }
+}
---
+++ CodeWars/UnitTests/MexicanWaveTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using KataSolutions.MexicanWave;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class MexicanWaveKataClass
+    {
+        [TestCase]
+        public void BasicTest1()
+        {
+            MexicanWaveKata kata = new MexicanWaveKata();
+            List<string> result = new List<string> { "Hello", "hEllo", "heLlo", "helLo", "hellO" };
+            Assert.AreEqual(result, kata.Wave("hello"), "it should return '" + result + "'");
+        }
+
+        [TestCase]
+        public void BasicTest2()
+        {
+            MexicanWaveKata kata = new MexicanWaveKata();
+            List<string> result = new List<string> { "Codewars", "cOdewars", "coDewars", "codEwars", "codeWars", "codewArs", "codewaRs", "codewarS" };
+            Assert.AreEqual(result, kata.Wave("codewars"), "it should return '" + result + "'");
+        }
+
+        [TestCase]
+        public void BasicTest3()
+        {
+            MexicanWaveKata kata = new MexicanWaveKata();
+            List<string> result = new List<string> { };
+            Assert.AreEqual(result, kata.Wave(""), "it should return '" + result + "'");
+        }
+
+        [TestCase]
+        public void BasicTest4()
+        {
+            MexicanWaveKata kata = new MexicanWaveKata();
+            List<string> result = new List<string> { "Two words", "tWo words", "twO words", "two Words", "two wOrds", "two woRds", "two worDs", "two wordS" };
+            Assert.AreEqual(result, kata.Wave("two words"), "it should return '" + result + "'");
+        }
+
+        [TestCase]
+        public void BasicTest5()
+        {
+            MexicanWaveKata kata = new MexicanWaveKata();
+            List<string> result = new List<string> { " Gap ", " gAp ", " gaP " };
+            Assert.AreEqual(result, kata.Wave(" gap "), "it should return '" + result + "'");
+        }
+
+    }
+}
