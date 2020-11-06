82c41735d46ce1b0a7fe76fb090f53d10461ea9a
Sean Owen <sean_owen@live.co.uk>
2020-11-06 21:10:16 +0000

    - completed WhereMyAnagramsAtKata

---
+++ CodeWars/ManualTesting/WhereMyAnagramsAt/WmaaKata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KataSolutions.WhereMyAnagramsAt
+{
+    public class WmaaKata
+    {
+        public static List<string> Anagrams(string word, List<string> words)
+        {
+            List<string> anagramList = new List<string>();
+            string wordArray = string.Concat(word.OrderBy(c => c));
+
+            foreach (string entry in words)
+            {
+                string orderedItem = string.Concat(entry.OrderBy(c => c));
+                if (orderedItem.Equals(wordArray))
+                {
+                    anagramList.Add(entry);
+                }
+            }
+            return anagramList;
+        }
+    }
+}
---
+++ CodeWars/UnitTests/WmaaTests.cs
@@ -0,0 +1,19 @@
+﻿using KataSolutions.WhereMyAnagramsAt;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class WmaaTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new List<string> { "a" }, WmaaKata.Anagrams("a", new List<string> { "a", "b", "c", "d" }));
+            Assert.AreEqual(new List<string> { "carer", "arcre", "carre" }, WmaaKata.Anagrams("racer", new List<string> { "carer", "arcre", "carre", "racrs", "racers", "arceer", "raccer", "carrer", "cerarr" }));           
+        }
+
+    }
+}
