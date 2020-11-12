84406c6858f99afa1475b0217f52d7ab051e96de
Sean <sean_owen@live.co.uk>
2020-11-12 23:58:59 +0000

    - completer sum strings as ints kata

---
+++ CodeWars/ManualTesting/SumStringsAsNumbers/SumStringsAsIntsKata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace KataSolutions.SumStringsAsNumbers
+{
+    public class SumStringsAsIntsKata
+    {
+        public static string sumStrings(string a, string b)
+        {
+            BigInteger.TryParse(a, out BigInteger i);
+            BigInteger.TryParse(b, out BigInteger j);
+
+            var output = i + j;
+            return $"{output}";
+        }
+    }
+}
---
+++ CodeWars/UnitTests/SumStringsAsIntsTests.cs
@@ -0,0 +1,18 @@
+﻿using KataSolutions.SumStringsAsNumbers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class SumStringsAsIntsTests
+    {
+        [Test]
+        public void Given123And456Returns579()
+        {
+            Assert.AreEqual("579", SumStringsAsIntsKata.sumStrings("123", "456"));
+        }
+    }
+}
