0beaf0803ede7f256c2d418f3a72b2dedc2f913f
Sean Owen <sean_owen@live.co.uk>
2020-11-13 23:22:29 +0000

    - solved parse int reloaded kata

---
+++ CodeWars/ManualTesting/ParseIntReloaded/ParseIntReloadedKata.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KataSolutions.ParseIntReloaded
+{
+    public class ParseIntReloadedKata
+    {
+        static Dictionary<string, int> textNumberPairs = new Dictionary<string, int>()
+        {
+            { "one", 1 },
+            { "two", 2 },
+            { "three", 3 },
+            { "four", 4 },
+            { "five", 5 },
+            { "six", 6 },
+            { "seven", 7 },
+            { "eight", 8 },
+            { "nine", 9 },
+
+            { "ten", 10 },
+            { "eleven", 11 },
+            { "twelve", 12 },
+            { "thirteen", 13 },
+            { "fourteen", 14 },
+            { "fifteen", 15 },
+            { "sixteen", 16 },
+            { "seventeen", 17 },
+            { "eighteen", 18 },
+            { "nineteen", 19 },
+            { "twenty", 20 },
+
+            { "thirty", 30 },
+            { "forty", 40 },
+            { "fifty", 50 },
+            { "sixty", 60 },
+            { "seventy", 70 },
+            { "eighty", 80 },
+            { "ninety", 90 },
+        };
+
+        static Dictionary<string, int> textNumberMultipliers = new Dictionary<string, int>()
+        {
+            { "hundred", 100 },
+
+            { "thousand", 1000 },
+
+            { "million", 1000000 },
+        };
+
+        // TODO - consider how this could be simplified
+        public static int ParseInt(string s)
+        {
+            string[] splitInput = s.Split(' ');
+            int output = 0;
+            int sumWithOutput = 0;
+            var usedMultipliers = new List<int>();
+
+            for (int i = 0; i < splitInput.Length; i++)
+            {
+                string valueLCase = splitInput[i].ToLower();
+                if (valueLCase != "and")
+                {
+                    string[] splitValue = valueLCase.Split('-');
+                    for (int j = 0; j < splitValue.Length; j++)
+                    {
+                        if (textNumberPairs.ContainsKey(splitValue[j]))
+                        {
+                            sumWithOutput += textNumberPairs[splitValue[j]];
+                        }
+
+                        if (textNumberMultipliers.ContainsKey(splitValue[j]))
+                        {
+                            int indexValue = textNumberMultipliers[splitValue[j]];
+                            if (usedMultipliers.FirstOrDefault(x => x > indexValue) != 0)
+                            {
+                                sumWithOutput *= textNumberMultipliers[splitValue[j]];
+                                output += sumWithOutput;
+                                sumWithOutput = 0;
+                            }
+                            else
+                            {
+                                usedMultipliers.Add(indexValue);
+
+                                output += sumWithOutput;
+                                sumWithOutput = 0;
+
+                                output = output * indexValue;
+                            }
+                        }
+                    }
+                }
+            }
+
+            output += sumWithOutput;
+            return output;
+        }
+    }
+}
---
+++ CodeWars/UnitTests/ParseIntReloadedTests.cs
@@ -0,0 +1,21 @@
+﻿using KataSolutions.ParseIntReloaded;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests
+{
+    class ParseIntReloadedTests
+    {
+        [Test]
+        public void FixedTests()
+        {
+            //    Assert.AreEqual(1, ParseIntReloadedKata.ParseInt("one"));
+            //    Assert.AreEqual(20, ParseIntReloadedKata.ParseInt("twenty"));
+            //    Assert.AreEqual(246, ParseIntReloadedKata.ParseInt("two hundred forty-six"));
+            Assert.AreEqual(1337, ParseIntReloadedKata.ParseInt("one thousand three hundred and thirty seven"));
+
+        }
+    }
+}
