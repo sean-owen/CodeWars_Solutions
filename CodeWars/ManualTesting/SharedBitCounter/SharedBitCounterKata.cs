d6dc05c9f9b87971a0022adf9bc4e0d3626e8306
Sean <sean_owen@live.co.uk>
2020-12-01 21:57:00 +0000

    - solved shared bit counter kata

---
+++ CodeWars/ManualTesting/SharedBitCounter/SharedBitCounterKata.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataSolutions.SharedBitCounter
+{
+    public static class SharedBitCounterKata
+    {
+        public static bool SharedBits(int a, int b)
+        {
+            int and = a & b;
+            if (and > 0)
+            {
+                int conditionMet = 0;
+                while (and > 0)
+                {
+                    conditionMet += and % 2;
+                    if (conditionMet > 1)
+                    {
+                        return true;
+                    }
+
+                    and /= 2;
+                }
+            }
+            return false;
+
+        }
+    }
+}
---
+++ CodeWars/UnitTests/SharedBitsTests.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using System;
+using KataSolutions.SharedBitCounter;
+
+using static KataSolutions.SharedBitCounter.SharedBitCounterKata;
+
+namespace UnitTests
+{
+    [TestFixture]
+    class SharedBitsTests
+    {
+        [Test]
+        public void SharedBits1()
+        {
+            //0 in common
+            Assert.AreEqual(false, SharedBits(1, 2));
+        }
+
+        [Test]
+        public void SharedBits2()
+        {
+            //0 in common
+            Assert.AreEqual(false, SharedBits(16, 8));
+        }
+        [Test]
+        public void SharedBits3()
+        {
+            //1 in common
+            Assert.AreEqual(false, SharedBits(1, 1));
+        }
+
+        [Test]
+        public void SharedBits4()
+        {
+            //1 in common
+            Assert.AreEqual(false, SharedBits(2, 3));
+        }
+
+        [Test]
+        public void SharedBits5()
+        {
+            //1 in common
+            Assert.AreEqual(false, SharedBits(7, 10));
+        }
+
+        [Test]
+        public void SharedBits6()
+        {
+            //2 in common
+            Assert.AreEqual(true, SharedBits(43, 77));
+        }
+
+        [Test]
+        public void SharedBits7()
+        {
+            //3 in common
+            Assert.AreEqual(true, SharedBits(7, 15));
+        }
+        [Test]
+        public void SharedBits8()
+        {
+            //3 in common
+            Assert.AreEqual(true, SharedBits(23, 7));
+        }
+
+    }
+
+}
