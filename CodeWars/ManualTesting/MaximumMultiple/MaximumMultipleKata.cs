8949cef5828bb4b58c384ee7aebec67ad744fe27
Sean <sean_owen@live.co.uk>
2020-12-03 23:05:42 +0000

    maximum multiply kata

---
+++ CodeWars/ManualTesting/MaximumMultiple/MaximumMultipleKata.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataSolutions.MaximumMultiple
+{
+    public static class MaximumMultipleKata
+    {
+        public static int MaxMultiply(int divisor, int bound)
+        {
+            //Do some magic
+            int multiplier = 0;
+            int maxDivisor = divisor;
+            while (maxDivisor < bound)
+            {
+                maxDivisor = multiplier * divisor;
+                multiplier++;
+            }
+
+            return maxDivisor == bound ? maxDivisor : maxDivisor - divisor;
+        }
+    }
+}
