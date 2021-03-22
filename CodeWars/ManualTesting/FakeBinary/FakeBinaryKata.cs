0119064e823eeb8a2d0e5f858afee6ed46781c1b
Sean Owen <sean_owen@live.co.uk>
2021-03-22 17:12:45 +0000

    FakeBinary kata

---
+++ CodeWars/ManualTesting/FakeBinary/FakeBinaryKata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataSolutions.FakeBinary
+{
+    public class FakeBinaryKata
+    {
+        public static string FakeBin(string x)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var letter in x)
+            {
+                if (Char.GetNumericValue(letter) < 5)
+                {
+                    sb.Append("0");
+                }
+                else
+                {
+                    sb.Append("1");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ CodeWars/UnitTests/FakeBinaryTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using KataSolutions.FakeBinary;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class FakeBinaryTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("01011110001100111", FakeBinaryKata.FakeBin("45385593107843568"));
+            Assert.AreEqual("101000111101101", FakeBinaryKata.FakeBin("509321967506747"));
+            Assert.AreEqual("011011110000101010000011011", FakeBinaryKata.FakeBin("366058562030849490134388085"));
+        }
+    }
+}
