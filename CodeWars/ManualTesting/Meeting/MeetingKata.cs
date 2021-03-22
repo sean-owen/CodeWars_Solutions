a6c6f915284d81b521950a49b38baed5324ebb39
Sean Owen <sean_owen@live.co.uk>
2021-03-22 19:28:24 +0000

    Meeting kata

---
+++ CodeWars/ManualTesting/Meeting/MeetingKata.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KataSolutions.Meeting
+{
+    public class MeetingKata
+    {
+        public static string Meeting(string s)
+        {
+            List<string> namesList = s.ToUpper().Split(';').ToList();
+            namesList.Sort();
+
+            var sortedNamesList = new List<string>();
+
+            foreach (var fullName in namesList)
+            {
+                string[] myArray = fullName.Split(':');
+                sortedNamesList.Add($"{myArray[1]}, {myArray[0]}");
+            }
+            sortedNamesList.Sort();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var fullName in sortedNamesList)
+            {
+                sb.Append($"({fullName})");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
