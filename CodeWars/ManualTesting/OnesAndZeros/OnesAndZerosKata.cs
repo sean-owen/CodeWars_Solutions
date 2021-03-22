85bdf6e42186562c46b3eb8992d87c64f1a84dda
Sean Owen <sean_owen@live.co.uk>
2021-03-22 17:37:46 +0000

    ones and zeros kata

---
+++ CodeWars/ManualTesting/OnesAndZeros/OnesAndZerosKata.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KataSolutions.OnesAndZeros
+{
+    public class OnesAndZerosKata
+    {
+        public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+            //Code here
+            int index = BinaryArray.Length - 1;
+            double output = 0;
+            foreach(int entry in BinaryArray)
+            {
+                if (entry == 1)
+                {
+                    output += Math.Pow(2, index);
+                }
+                index--;
+            }
+
+            return (int)output;
+        }
+    }
+}
---
+++ CodeWars/UnitTests/OnesAndZerosTests.cs
@@ -0,0 +1,23 @@
+﻿using KataSolutions.OnesAndZeros;
+using NUnit.Framework;
+using System;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class OnesAndZerosTests
+    {
+        int[] Test1 = new int[] { 0, 0, 0, 0 };
+        int[] Test2 = new int[] { 1, 1, 1, 1 };
+        int[] Test3 = new int[] { 0, 1, 1, 0 };
+        int[] Test4 = new int[] { 0, 1, 0, 1 };
+        [Test]
+        public void BasicTesting()
+        {
+            Assert.AreEqual(0, OnesAndZerosKata.binaryArrayToNumber(Test1));
+            Assert.AreEqual(15, OnesAndZerosKata.binaryArrayToNumber(Test2));
+            Assert.AreEqual(6, OnesAndZerosKata.binaryArrayToNumber(Test3));
+            Assert.AreEqual(5, OnesAndZerosKata.binaryArrayToNumber(Test4));
+        }
+    }
+}
